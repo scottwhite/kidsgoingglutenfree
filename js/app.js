d27e46e7e7026cd21a9fd2a9b34d56dd9e4d75b4
Scott White <scott@3rdarm.com>
2012-11-18 20:31:06 -0500

    initial add

---
+++ js/app.js
@@ -0,0 +1,12 @@
+angular.module('kggf', ['kggf_services'])
+
+function AppCtrl($scope) {
+  $scope.phones = [
+    {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S."},
+    {"name": "Motorola XOOM™ with Wi-Fi",
+     "snippet": "The Next, Next Generation tablet."},
+    {"name": "MOTOROLA XOOM™",
+     "snippet": "The Next, Next Generation tablet."}
+  ];
+}
